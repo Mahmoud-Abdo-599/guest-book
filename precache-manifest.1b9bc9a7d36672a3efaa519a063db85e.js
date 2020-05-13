caadbd2788c75ad27bf76c433c98c11106302985
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-05-13 07:16:09 +0200

    Updates

---
+++ precache-manifest.1b9bc9a7d36672a3efaa519a063db85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403f63f02958e904efd5b629b62fd40a",
+    "url": "/guest-book/index.html"
+  },
+  {
+    "revision": "5c4231b01e845c053fbe",
+    "url": "/guest-book/static/css/main.22af089a.chunk.css"
+  },
+  {
+    "revision": "d16ff27f8e5e41e3a3c6",
+    "url": "/guest-book/static/js/2.55ce355c.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/guest-book/static/js/2.55ce355c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4231b01e845c053fbe",
+    "url": "/guest-book/static/js/main.b3985afe.chunk.js"
+  },
+  {
+    "revision": "c97d1a4882c53e5786d1",
+    "url": "/guest-book/static/js/runtime-main.0c1a0c7d.js"
+  }
+]);
